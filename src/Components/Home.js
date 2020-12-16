b531efe8d8b579c7cf3c17cb09672daff5419c43
Syeda Umme Hani <36325175+hani29@users.noreply.github.com>
2020-12-16 10:57:22 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -13,20 +13,6 @@ var settings = {
     autoplaySpeed: 5,
     pauseOnHover: true,
     arrows: false,
-    // responsive: [
-    //   {
-    //     breakpoint: 320,
-    //     settings: { slidesToShow: 1, slidesToScroll: 1, infinite: false }
-    //   },
-    //   {
-    //     breakpoint: 768,
-    //     settings: { slidesToShow: 2, slidesToScroll: 2, infinite: false }
-    //   },
-    //   {
-    //     breakpoint: 1024,
-    //     settings: { slidesToShow: 3, slidesToScroll: 3, infinite: false }
-    //   }
-    // ]
     responsive: [{ breakpoint: 980, settings: { slidesToShow: 3, slidesToScroll: 3, initialSlide: 3 } }, { breakpoint: 600, settings: { slidesToShow: 2, slidesToScroll: 2 } }]
 };
 
